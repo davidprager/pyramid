a11906955d8b39a373d75a927ec2d2246440c971
davidprager <david_prager@yahoo.com>
2021-08-19 22:30:39 -0400

    Initial load of project

---
+++ Pyramid.js
@@ -0,0 +1,27 @@
+/*
+ * File: pyramid.js
+ * -----------------------
+ * The program uses nested loops to draw a pyramid on the console.
+ */
+
+"use strict";
+
+/*
+ * Draws a pyramid on the console.  The parameter is the number of levels
+ * in the pyramid.
+ */
+
+function drawPyramid(height) {
+
+    // The following line is provided for you to ensure that the structure of the project is set up correctly
+    // It should display the height of the pyramid as specified on line 8 of the HTML file
+    // Once you are satisfied that the project is loaded and working OK, you can delete these comments and the next line
+    // and write your program.
+    console.log(height);
+
+    //TODO : Complete function
+
+
+
+}
+
